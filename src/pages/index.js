7d28f5c09f31ece0bb71789dda3c033fc036d3ea
Kumar Harsh <kharsh39@gmail.com>
2023-01-11 08:44:56 +0530

    Update homepage and README

--- src/pages/index.js
+++ src/pages/index.js
@@ -132,8 +132,7 @@ const IndexPage = () => {
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
+        Hello world! 😎
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
